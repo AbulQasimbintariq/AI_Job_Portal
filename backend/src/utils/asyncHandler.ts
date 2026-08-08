af8f0f6ee93244eecbdb3d0b33982f0f907fd5eb
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2026-08-09 03:10:10 +0500

    for deploy

--- backend/src/utils/asyncHandler.ts
+++ backend/src/utils/asyncHandler.ts
@@ -1,9 +1,30 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
+// import { Request, Response, NextFunction, RequestHandler } from "express";
+
+// export const asyncHandler = (
+//     fn: RequestHandler
+// ): RequestHandler => {
+//     return (req: Request, res: Response, next: NextFunction) => {
+//         Promise.resolve(fn(req, res, next)).catch(next);
+//     };
+// };
+
+
+import { Request, Response, NextFunction } from "express";
+
+type AsyncController = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => Promise<Response | void>;
 
 export const asyncHandler = (
-    fn: RequestHandler
-): RequestHandler => {
-    return (req: Request, res: Response, next: NextFunction) => {
+    fn: AsyncController
+) => {
+    return (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): void => {
         Promise.resolve(fn(req, res, next)).catch(next);
     };
 };
