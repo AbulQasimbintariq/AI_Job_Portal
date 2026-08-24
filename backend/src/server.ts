eda386d640b489d538cb0fc1edb30314972712d7
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2026-08-25 01:12:59 +0500

    fix error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,10 @@ dotenv.config();
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.NEXT_PUBLIC_SITE_URL;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`🚀 Server running on http://localhost:${PORT}`);
+        console.log(`🚀 Server running on https://ai-job-portal-nu-gold.vercel.app/`);
     });
 });
